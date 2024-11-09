abc2b3f2667c8ad4f7e4621ff253f80708a61a87
Patrick Connolly <patrick.c.connolly@gmail.com>
2024-11-08 23:10:02 -0800

    Oops. Added lint fixes.

--- src/js/app.js
+++ src/js/app.js
@@ -42,14 +42,14 @@ if (search) {
   search.classList.remove('dn')
   const input = search.querySelector('input')
 
-  function setSearchParam(searchParam) {
+  function setSearchParam (searchParam) {
     // Update the URL without adding to the search history
     const newurl = `${window.location.protocol}//${window.location.host}${window.location.pathname}?q=${encodeURIComponent(searchParam)}`
-    window.history.replaceState({path: newurl}, '', newurl)
+    window.history.replaceState({ path: newurl }, '', newurl)
   }
 
   // Function to handle search logic
-  function handleSearch(searchValue) {
+  function handleSearch (searchValue) {
     setSearchParam(searchValue)
 
     if (searchValue === '') {
